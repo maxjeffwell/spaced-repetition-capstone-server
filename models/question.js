2ee3f3952a1272281c0626d630c264765c17d73a
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-16 14:58:02 -0700

    created questions model and get routes

---
+++ models/question.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require ('mongoose');
+
+const questionSchema = new mongoose.Schema ({
+  question: { type: String, required: true },
+  answer: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Question', questionSchema);
+
